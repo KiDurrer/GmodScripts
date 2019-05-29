e15d216b0a15f3ace00ec31cbc14b831515d1020
Ki Durrer <Durrerki@gmail.com>
2019-05-29 10:51:33 -0500

    Create pmcustom.lua

---
+++ pmcustom.lua
@@ -0,0 +1,33 @@
+-- File in the server autorun
+net.AddNetworkMessage("pmcustom")
+
+hook.Add("PlayerSay","NetworkTest, function(ply, test, boolT)
+  if(string.sub(string.lower(text), 1, 3) == "!pm") then
+    local isFound = false
+    local messageTable = string.Split(string.lower(text), " ")
+    local message = string.sub(text, string.len(messageTable[2])+4)
+    for k, v in pairs( player.GetAll() ) do
+      if string.find(string.lower(v:Nick()), messageTable[2]) ) then
+        net.Start("pmcustom")
+          net.WriteString(ply:Nick())
+          net.WriteString(message)
+        net.Send(v)
+        ply:ChatPrint("pm sent to "..v:Nick())
+        return ""
+      end 
+    end
+    if isFound == false then
+      ply:ChatPrint("Player not found")
+      return ""
+    end
+  end
+end)
+
+-- File in the client autorun
+
+net.Receive("pmcustom", function(len)
+  local ply = LocalPlayer()
+  local sender = net.ReadString()
+  local message = net.ReadString()
+  ply:ChatPrint()
+end)
