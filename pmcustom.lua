c8e648fecf12b2a5de1ae3969cb679279831f936
Ki Durrer <Durrerki@gmail.com>
2019-05-29 10:59:32 -0500

    Update pmcustom.lua

--- pmcustom.lua
+++ pmcustom.lua
@@ -5,13 +5,14 @@ hook.Add("PlayerSay","NetworkTest, function(ply, test, boolT)
   if(string.sub(string.lower(text), 1, 3) == "!pm") then
     local isFound = false
     local messageTable = string.Split(string.lower(text), " ")
-    local message = string.sub(text, string.len(messageTable[2])+4)
+    local message = string.sub(text, string.len(messageTable[2])+4) or " "
     for k, v in pairs( player.GetAll() ) do
       if string.find(string.lower(v:Nick()), messageTable[2]) ) then
         net.Start("pmcustom")
           net.WriteString(ply:Nick())
           net.WriteString(message)
         net.Send(v)
+        isFound = true
         ply:ChatPrint("pm sent to "..v:Nick())
         return ""
       end 
