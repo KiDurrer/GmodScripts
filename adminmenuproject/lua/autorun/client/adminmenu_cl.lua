6499c926eb00a0986fc4b63dd136964d6a69a1e9
Ki Durrer <Durrerki@gmail.com>
2019-05-29 14:24:42 -0500

    Update adminmenu_cl.lua

--- adminmenuproject/lua/autorun/client/adminmenu_cl.lua
+++ adminmenuproject/lua/autorun/client/adminmenu_cl.lua
@@ -4,6 +4,10 @@ net.Receive("adminmenuNet", function()
 	frame:SetSize(400,400)
 	frame:Center()
 	frame:SetTitle("Admin-Panel - "..ply:Nick())
+	frame:MakePopup()
+
+	
+	local labelH1 = vgui.Create("")
 	
 	local button1 = vgui.Create("DButton" frame)
 end)
